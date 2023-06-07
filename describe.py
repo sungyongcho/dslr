45492c1069875ea6939432f8a3f0049110917144
Sungyong Cho <here@sungyongcho.com>
2023-06-07 21:23:07 +0200

    chore: add comment, change file name

--- describe.py
+++ describe.py
@@ -6,25 +6,40 @@ import pandas as pd
 
 def describe_dataset(filename):
     try:
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename).set_index('Index')
         numeric_features = df.select_dtypes(include='number')
+        summary_df = pd.DataFrame()
+        for column in numeric_features.columns:
+            values = df[column]
+            # to drop nan values in order to handle data properly
+            values = values.dropna()
+            # Calculate statistics manually
+            count = len(values)
+            mean = sum(values) / count
+            stddev = (sum((x - mean) ** 2 for x in values) / count) ** 0.5
+            minimum = min(values)
+            quartiles = [values.quantile(q) for q in [0.25, 0.50, 0.75]]
+            maximum = max(values)
 
-        print("OG Data:", df.shape[1],
-              "numeric only:", numeric_features.shape[1])
-        if numeric_features.empty:
-            print("No numerical features found in the dataset.")
-            return
+            # Create a temporary DataFrame with custom index
+            index_labels = ['Count', 'Mean', 'Std',
+                            'Min', '25%', '50%', '75%', 'Max']
+            temp_df = pd.DataFrame([count, mean, stddev, minimum, quartiles[0],
+                                    quartiles[1], quartiles[2], maximum],
+                                   index=index_labels, columns=[column])
 
-        description = numeric_features.describe()
-        print(description)
+            # Append the temporary DataFrame to the summary DataFrame
+            summary_df = pd.concat([summary_df, temp_df], axis=1)
 
+        print(summary_df)
+        # to compare result with pandas describe funciton
+        # print(numeric_features.describe())
     except FileNotFoundError:
         print("File not found. Please provide a valid filename.")
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python describe.py [dataset_filename]")
-    else:
-        dataset_filename = sys.argv[1]
-        describe_dataset(dataset_filename)
+if len(sys.argv) != 2:
+    print("Usage: python describe.py [dataset_filename]")
+else:
+    dataset_filename = sys.argv[1]
+    describe_dataset(dataset_filename)
