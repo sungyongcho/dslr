f3edc7abcd4126074b865fdebc5d817fa0474611
Sungyong Cho <here@sungyongcho.com>
2023-07-15 12:27:23 +0200

    fix: pair plot done, change to output png file

--- pair.py
+++ pair.py
@@ -6,15 +6,44 @@ import seaborn as sns
 
 
 def pair(data):
-    # # Step 3: Create pair plot or scatter plot matrix for categorical features
-    # Adjust the size according to your desired pixel values
+    # Step 3: Create pair plot or scatter plot matrix for categorical features
+    houses = data['Hogwarts House'].unique()
+    colors = {'Ravenclaw': '#2b7bba', 'Slytherin': '#138b4a',
+              'Gryffindor': '#c72c41', 'Hufflepuff': '#e9c24d'}
 
-    sns.pairplot(data, kind='scatter', diag_kind='hist',
-                 height=0.8, aspect=1.2, hue='Hogwarts House')
-    plt.show()
+    # Increase the size of the figure and adjust padding/margins
+    sns.set(font_scale=1.0)
+    plt.figure(figsize=(16, 12))  # Increase the size as desired
+    # Adjust the size of the dots
+    plot_kws = {'s': 15}  # Modify the size as desired
+
+    # Rotate the y-labels
+    g = sns.pairplot(data, kind='scatter', diag_kind='hist',
+                     height=1.5, aspect=1.5, hue='Hogwarts House', palette=colors, plot_kws=plot_kws)
+
+    for ax in g.axes.flat:
+        ax.set_ylabel(ax.get_ylabel(), rotation=45,
+                      horizontalalignment='right')
+        ax.set_xlabel(ax.get_xlabel(), rotation=45,
+                      horizontalalignment='right')
+
+    # Adjust padding/margins for y-axis labels
+    g.fig.subplots_adjust(left=0.08, bottom=0.12)
+
+    # Save the pair plot as an image
+    plt.savefig('pair.png', dpi=300)
+
+    # if you want to see
+    # plt.show()
+
+    print('done')
 
 
 if __name__ == "__main__":
     data = pd.read_csv('./datasets/dataset_train.csv').set_index('Index')
+    print(data.columns)
+
     numeric_features = data.select_dtypes(include='number').columns
+    numeric_features = numeric_features.append(
+        pd.Index([data['Hogwarts House']]))
     pair(data)
