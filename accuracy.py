78938f9480a8ec2dc9b3cb4f91576c6e5689b467
Sungyong Cho <to.sungyongcho@gmail.com>
2023-07-21 21:41:52 +0200

    feat: main functionality workig; done

--- accuracy.py
+++ accuracy.py
@@ -3,9 +3,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 # Read the merged data (predicted) and ground truth data from CSV files
-merged_data = pd.read_csv('merged_data.csv', header=None, squeeze=True)
+merged_data = pd.read_csv('houses.csv', header=0, index_col=0, squeeze=True)
 ground_truth = pd.read_csv(
-    'dataset_truth.csv', header=0, index_col=0, squeeze=True)
+    './datasets/dataset_truth.csv', header=0, index_col=0, squeeze=True)
 
 # Calculate accuracy
 accuracy = classification_report(ground_truth, merged_data)
--- logreg_predict.py
+++ logreg_predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from minmax import minmax
 from my_logistic_regression import MyLogisticRegression
+from logreg_train import normalization
 
 if __name__ == "__main__":
     data_predict = pd.read_csv(
@@ -24,34 +25,36 @@ if __name__ == "__main__":
         column_mask = np.isnan(numeric_predict[:, i])
         numeric_predict[column_mask, i] = column_means[i]
 
-    numeric_predict = minmax(numeric_predict)
+    # numeric_predict = minmax(numeric_predict)
+    numeric_predict = normalization(numeric_predict)
+    # print(numeric_predict[0])
 
     num_classes = len(weights)
 
     result_array = np.array([])  # Initialize an empty array
     for i in range(num_classes):
         theta = weights.iloc[i].values.tolist()
-        theta_nested = [[value] for value in theta]
-        theta_nested.pop(0)
-        # print(classzero_nested)
+        # theta_nested = [[value] for value in theta]
+        # theta_nested.pop(0)
+        # print(theta_nested)
 
-        mylr = MyLogisticRegression(theta_nested)
+        mylr = MyLogisticRegression(np.array(theta[1:]), 0.1, 1000, None, 0.0, 'batch')
 
         # print(len(numeric_predict))
         # # print(len(classzero_nested))
 
-        y_pred = mylr.predict_(numeric_predict)
+        y_pred = mylr.predict_(numeric_predict[0])
 
-        y_pred_binary = (y_pred >= 0.5).astype(int).flatten()
+        y_pred_binary = (y_pred >= 0.5).astype(int)
         y_pred_binary = np.reshape(y_pred_binary, (-1, 1)).astype(str)
 
         for sublist in y_pred_binary:
             if int(sublist[0]) == 1:
                 sublist[0] = label_map[i]
 
-        filename = 'result_' + label_map[i] + '.csv'
-        np.savetxt(filename,
-                   y_pred_binary, delimiter=',', fmt='%s')
+        # filename = 'result_' + label_map[i] + '.csv'
+        # np.savetxt(filename,
+        #            y_pred_binary, delimiter=',', fmt='%s')
 
         # print(y_pred_binary)
         if result_array.size == 0:
@@ -60,4 +63,19 @@ if __name__ == "__main__":
             result_array = np.hstack((result_array, y_pred))
 
     print(result_array)
-    np.savetxt('result.csv', result_array, delimiter=',', fmt='%s')
+    # Define the label map
+    label_map = {0: 'Ravenclaw', 1: 'Slytherin',
+                 2: 'Gryffindor', 3: 'Hufflepuff'}
+
+    # Process each row and find the highest value column
+    result_df = pd.DataFrame(result_array)
+
+    highest_labels = result_df.idxmax(axis=1).map(label_map)
+
+    highest_labels = pd.DataFrame({"Hogwarts House": highest_labels})
+
+    print(highest_labels)
+    # Write the result to a new CSV file without the header
+    highest_labels.to_csv('houses.csv', index_label='Index', header=True)
+
+    # np.savetxt('result.csv', result_array, delimiter=',', fmt='%s')
